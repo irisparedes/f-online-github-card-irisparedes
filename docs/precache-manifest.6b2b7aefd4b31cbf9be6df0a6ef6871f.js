b7d77ec9c8ad9c6250920a0fcc4c4cb98f02d328
iris paredes <irisparedesjimenez@gmail.com>
2019-07-28 20:15:32 +0200

    add docs folder

---
+++ docs/precache-manifest.6b2b7aefd4b31cbf9be6df0a6ef6871f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a945d9d19ca41fbb09ae6034cf0b1b2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82e956ff25e1824174ad",
+    "url": "./static/css/main.958cee62.chunk.css"
+  },
+  {
+    "revision": "406ef74bde4d6dfafb34",
+    "url": "./static/js/2.853e471b.chunk.js"
+  },
+  {
+    "revision": "82e956ff25e1824174ad",
+    "url": "./static/js/main.d3181cca.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "471b278da4c31b13e10f07ce7a1613a4",
+    "url": "./static/media/logo-adalab.471b278d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
