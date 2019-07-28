a67da35b37e44ad5d2421101cafc1cd3011634e3
iris paredes <irisparedesjimenez@gmail.com>
2019-07-28 20:05:53 +0200

    add basic readme

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
-// import './styles/styles.scss';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
